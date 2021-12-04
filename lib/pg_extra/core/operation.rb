455fd93fdbb991f82962b714e68e8b0839d7e01a
Andrew Kozin <andrew.kozin@gmail.com>
2021-12-04 22:33:13 +0300

    Add base class for operations
    To define a new operation all you should do
    is to inherit a subclass from the `PGExtra::Operation`
    and specify its various aspects like:
    
    - the operation's `attributes`
    - their `validations`
    - the list of `ruby_params` (to enable sending some attributes as
    positional parameters of the corresponding method)
    - the `ruby_snippet` (to convert operation to ruby dump)
    - the `to_sql` (to provide a request for the operation's execution)

---
+++ lib/pg_extra/core/operation.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: false
+
+module PGExtra
+  # @api private
+  # Base class for operations.
+  # Inherit this class to define new operation.
+  # rubocop: disable Lint/EmptyClass
+  class Operation
+  end
+  # rubocop: enable Lint/EmptyClass
+end
